e3b96df6ee636c184883201dc8c0bc2184670073
Abhishek Bochare <asbochare2005@gmail.com>
2026-06-05 23:12:46 +0530

    add addDigits function implementation

---
+++ Day_5.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int addDigits(int num) {
+        if (num == 0) return 0;
+        return 1 + (num - 1) % 9;
+    }
+};
