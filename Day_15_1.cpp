f6b758c2dba9e84e11c109ba326733345283cc6e
Abhishek Bochare <asbochare2005@gmail.com>
2026-06-15 22:42:39 +0530

    Day 15

---
+++ Day_15_1.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+      ListNode* first = head;
+        for (int i = 1; i < k; i++) {
+            first = first->next;
+        }
+
+        ListNode* kthFromStart = first;
+        ListNode* second = head;
+
+        while (first->next) {
+            first = first->next;
+            second = second->next;
+        }
+
+        swap(kthFromStart->val, second->val);
+
+        return head;   
+    }
+};
